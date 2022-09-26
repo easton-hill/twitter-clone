1a2f447f04b4abf02d3a5405b02f865c24385886
easton-hill <exh424@gmail.com>
2022-09-26 15:14:49 -0400

    change click behavior of trends

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -20,9 +20,14 @@ const TrendCard = ({ trend }: TrendCardProps): ReactElement => {
   }
 
   return (
-    <div className='border-b border-off-white p-4' onClick={handleClick}>
-      <h1 className={`text-4xl cursor-pointer ${displayTrendingTweets && !trend.tweet_volume? 'mb-2': ''}`}>{trend.name}</h1>
-      {trend.tweet_volume && <h2 className={`text-xl cursor-pointer ${ displayTrendingTweets ? 'mb-2': ''}`}>{formatNumber(trend.tweet_volume)} tweets</h2>}
+    <div className='border-b border-off-white p-4'>
+      <h1 onClick={handleClick} className={`text-4xl cursor-pointer ${displayTrendingTweets && !trend.tweet_volume? 'mb-2': ''}`}>
+        {trend.name}
+      </h1>
+      {trend.tweet_volume &&
+        <h2 onClick={handleClick} className={`text-xl cursor-pointer ${ displayTrendingTweets ? 'mb-2': ''}`}>
+          {formatNumber(trend.tweet_volume)} tweets
+        </h2>}
       {displayTrendingTweets ? trendingTweets.map((tweet: Tweet) => (
         <TweetCard key={tweet.id} tweet={tweet} />
       )) : undefined}
